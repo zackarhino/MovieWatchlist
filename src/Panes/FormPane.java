a3230dc97acaff6c450873d6b727ff086720e590
Jenny Hoang <jennyhoang_9@hotmail.com>
2020-11-05 16:26:50 -0500

    #2 Production Company and Genre were changed to ComboBox

--- src/Panes/FormPane.java
+++ src/Panes/FormPane.java
@@ -1,7 +1,9 @@
 package Panes;
 
+import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -9,11 +11,13 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+
 public class FormPane extends BorderPane {
     private TextField movieName = new TextField();
     private TextField year = new TextField();
-    private TextField productionCompany = new TextField();
-    private TextField genre = new TextField();
+    private ComboBox productionCompany = new ComboBox();
+    private ComboBox genre = new ComboBox();
 
     public FormPane(){
         HBox input1 = new HBox();
@@ -21,8 +25,22 @@ public class FormPane extends BorderPane {
                 movieName, new Label("Year: "), year);
         input1.setAlignment(Pos.CENTER);
 
+        //genres
+        ArrayList<String> genres = new ArrayList<>();
+        genres.add("Action");
+        genres.add("Comedy");
+        genres.add("Horror");
+        genre.setItems(FXCollections.observableArrayList(genres));
+
+        //production companies
+        ArrayList<String> productionCompanies = new ArrayList<>();
+        productionCompanies.add("Marvel");
+        productionCompanies.add("Universal Studios");
+        productionCompanies.add("Warner Bros.");
+        productionCompany.setItems(FXCollections.observableArrayList(productionCompanies));
+
         HBox input2 = new HBox();
-        input2.getChildren().addAll(new Label("Production Company"), productionCompany,
+        input2.getChildren().addAll(new Label("Production Company: "), productionCompany,
                 new Label("Genre: "), genre);
         input2.setAlignment(Pos.CENTER);
 
@@ -40,8 +58,6 @@ public class FormPane extends BorderPane {
                 //genre.getText()
                 movieName.clear();
                 year.clear();
-                productionCompany.clear();
-                genre.clear();
         });
 
         this.setCenter(inputs);
