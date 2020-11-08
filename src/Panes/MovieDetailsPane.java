6fd99456fd748cdac17f7d2890748a42d57065d9
Trevor Slobodnick <W0740591@myscc.ca>
2020-11-08 00:11:15 -0500

    #4 Added all widgets needed - All the widgets that are necessary for Movie Details page to function have been added - small_star.png was added to the images because star.png is too large, it is the same image as star.png, just resized to be 64 pixels wide

--- src/Panes/MovieDetailsPane.java
+++ src/Panes/MovieDetailsPane.java
@@ -16,10 +16,11 @@ public class MovieDetailsPane extends BorderPane {
         //Entire Screen
         VBox screenWrapper = new VBox();
         //Top Section
+        HBox topSection = new HBox();
         HBox watchedWrapper = new HBox();
         Text watchedText = new Text("Watched");
         CheckBox watchedCheckbox = new CheckBox();
-        Button close = new Button("X");
+        Button closeBtn = new Button("X");
         //Rest of Screen
         HBox lrWrapper = new HBox();
         //Left side
@@ -32,16 +33,34 @@ public class MovieDetailsPane extends BorderPane {
         //Right side
         VBox rightSideContainer = new VBox();
         HBox ratingContainer = new HBox();
-        Image star1 = new Image("Images/star.png");
+        Image star1 = new Image("Images/small_star.png");
         ImageView rating1 = new ImageView(star1);
-        Image star2 = new Image("Images/star.png");
+        Image star2 = new Image("Images/small_star.png");
         ImageView rating2 = new ImageView(star2);
-        Image star3 = new Image("Images/star.png");
+        Image star3 = new Image("Images/small_star.png");
         ImageView rating3 = new ImageView(star3);
-        Image star4 = new Image("Images/star.png");
+        Image star4 = new Image("Images/small_star.png");
         ImageView rating4 = new ImageView(star4);
-        Image star5 = new Image("Images/star.png");
+        Image star5 = new Image("Images/small_star.png");
         ImageView rating5 = new ImageView(star5);
+        Button removeMovieBtn = new Button("Remove From Watchlist");
+
+        //Putting It All Together...
+        //Top Section
+        watchedWrapper.getChildren().addAll(watchedText, watchedCheckbox);
+        topSection.getChildren().addAll(watchedWrapper, closeBtn);
+        //Left Side
+        additionalInfo.getChildren().addAll(movieYear, movieProdCompany);
+        leftSideContainer.getChildren().addAll(movieTitle, movieGenre, additionalInfo);
+        //Right Side
+        ratingContainer.getChildren().addAll(rating1, rating2, rating3, rating4, rating5);
+        rightSideContainer.getChildren().addAll(ratingContainer, removeMovieBtn);
+        //Wrap Left and Right Sides
+        lrWrapper.getChildren().addAll(leftSideContainer, rightSideContainer);
+        //Adding final containers to screen
+        screenWrapper.getChildren().addAll(topSection, lrWrapper);
+
+        pane.setCenter(screenWrapper);
 
         this.setCenter(pane);
     }
