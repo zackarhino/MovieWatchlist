c3520b096c3c769592a16df6f0df82df9ef44679
Trevor Slobodnick <W0740591@myscc.ca>
2020-11-08 14:44:30 -0500

    #3 Linked ComboBox to Text value - When a value is selected in combobox (need to watch or watched), the header text will update to reflect the changes - set the default value of combobox to the first item in the arraylist

--- src/Panes/ViewWatchlistPane.java
+++ src/Panes/ViewWatchlistPane.java
@@ -1,10 +1,34 @@
 package Panes;
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
 
 public class ViewWatchlistPane extends BorderPane {
     public ViewWatchlistPane(){
         BorderPane pane = new BorderPane();
+
+        ArrayList<String> status = new ArrayList<>();
+        status.add("Need to Watch");
+        status.add("Watched");
+
+        VBox watchlistWrapper = new VBox();
+        HBox title = new HBox(new Text("My Movies"));
+        HBox selectStatusContainer = new HBox();
+        ComboBox<String> selectStatus = new ComboBox<>(FXCollections.observableArrayList(status));
+        selectStatus.getSelectionModel().selectFirst();
+        Text statusTxt = new Text();
+        HBox statusTxtContainer = new HBox(statusTxt);
+
+        selectStatus.setOnAction(actionEvent -> statusTxt.setText(selectStatus.getValue()));
+
         this.setCenter(pane);
     }
 }
