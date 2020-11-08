bf8ad16beb39e612d60c459365322e69e1680374
Trevor Slobodnick <W0740591@myscc.ca>
2020-11-07 22:10:27 -0500

    #4 Built basic classes for Movie Details - Added an empty pane in the Panes folder - Added the MovieDetailsScene in the Scenes folder

---
+++ src/Panes/MovieDetailsPane.java
@@ -0,0 +1,10 @@
+package Panes;
+
+import javafx.scene.layout.BorderPane;
+
+public class MovieDetailsPane extends BorderPane {
+    public MovieDetailsPane(){
+        BorderPane pane = new BorderPane();
+        this.setCenter(pane);
+    }
+}
