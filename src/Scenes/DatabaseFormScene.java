bbc2c50396062efe73207a67445c766b98c6bad1
Jenny Hoang <jennyhoang_9@hotmail.com>
2020-11-19 15:38:08 -0500

    Database Form Created and Functional

--- src/Scenes/DatabaseFormScene.java
+++ src/Scenes/DatabaseFormScene.java
@@ -4,15 +4,17 @@ import Panes.DatabaseFormPane;
 import Util.Constants;
 import javafx.scene.Scene;
 
+import java.io.IOException;
+
 public class DatabaseFormScene extends Scene {
     private static DatabaseFormScene databaseFormScene;
 
-    private DatabaseFormScene() {
+    public DatabaseFormScene() throws IOException {
         super(new DatabaseFormPane(), Constants.screenWidth, Constants.screenHeight);
     }
 
     // Singleton Design Pattern
-    public static DatabaseFormScene getInstance(){
+    public static DatabaseFormScene getInstance() throws IOException {
         if(databaseFormScene == null){
             databaseFormScene = new DatabaseFormScene();
         }
