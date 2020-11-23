26b8dfcbe814144755eebe8afd7583f474f2d314
Trevor Slobodnick <W0740591@myscc.ca>
2020-11-23 14:18:24 -0500

    Created Database classes - Added Database/Const and Credentials/config.txt to gitignore

---
+++ src/Database/Const.java
@@ -0,0 +1,7 @@
+package Database;
+
+public class Const {
+    public static final String DB_NAME = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASS = "";
+}
