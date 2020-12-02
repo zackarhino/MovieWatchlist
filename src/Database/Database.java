918187cbec0ac12860c6b8eaeee21755d5846bcf
Zachary Allard <zackallard17@gmail.com>
2020-12-02 16:12:00 -0500

    Revert "Revert "Bug Fixes/Refactoring""
    This reverts commit d7f7b2ee4383ab18e0ff80eed11e8f1bce6912c0.

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -213,8 +213,8 @@ public class Database {
             ResultSet rs = statement.executeQuery(query);
             rs.next();
             return rs.getString(1);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return "";
     }
