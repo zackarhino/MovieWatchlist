83b45a43dd8b7ade42667201b2001b858e3597f9
Zachary Allard <zackallard17@gmail.com>
2020-12-03 14:21:59 -0500

    #15 Updated graph * Changed colors and sizing * Fixed display issue

--- src/Panes/StatsPane.java
+++ src/Panes/StatsPane.java
@@ -6,52 +6,69 @@ import Movie.Movie;
 import Scenes.MenuScene;
 import Tables.MovieTable;
 import Util.Constants;
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
 
+/**
+ * Pane for displaying watch stats
+ * @author Jenny Hoang, Zachary Allard
+ */
 public class StatsPane extends BorderPane {
     private PieChart chart;
 
     public StatsPane(){
+        HBox buttons = new HBox();
         Button backButton = new Button("< Back");
+        buttons.getChildren().addAll(backButton);
+        buttons.setPadding(new Insets(Constants.DEFAULT_PADDING));
         backButton.setOnAction(actionEvent -> Main.switchScene(MenuScene.getInstance()));
-        this.setBackground(new Background(new BackgroundFill(Constants.COLOR_BACKGROUND, null, null)));
-        this.setBottom(backButton);
 
-        chart = new PieChart();
-        chart.setTitle("Watched Movies");
-        chart.setLabelsVisible(true);
-        pieChart();
-        this.setCenter(chart);
+        this.chart = createPieChart();
+        this.chart.setPrefWidth(Constants.SCREEN_WIDTH);
+
+        Group group = new Group(this.chart);
+
+        this.setBackground(new Background(new BackgroundFill(Constants.COLOR_BACKGROUND, null, null)));
+        this.setBottom(buttons);
+        this.setCenter(group);
     }
 
-    public void pieChart(){
-        //Access to database
-        MovieTable movieTable = new MovieTable();
+    /**
+     * Create a new PieChart and return it
+     * @return PieChart of local data
+     * @author Jenny Hoang
+     */
+    private PieChart createPieChart(){
         //Grab list of movies
-        ArrayList<Movie> movies = movieTable.getAllMovies();
-        //Clear data in chart
-        chart.getData().clear();
+        ArrayList<Movie> movies = Movie.getTotalMovies();
+
         //Build list of pieChart data
-        ArrayList<PieChart.Data> data = new ArrayList<>();
-        for(Movie movie : movies){
-            //dont add record unless there is more than 1 movie
-            if(movieTable.getMovieCount(movie.getId()) > 0){
-                data.add(new PieChart.Data(movie.getTitle(),
-                        movieTable.getMovieCount(movie.getId())));
+        ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
+        //dont add record unless there is more than 1 movie
+        if(!movies.isEmpty()){
+            for(Movie movie : movies){
+                // TODO: display something else
+                System.out.println(movie.getTitle() + ", id: " + movie.getId());
+                data.add(new PieChart.Data(movie.getTitle(), movie.getId()));
             }
         }
-        //Wrapping
-        ObservableList<PieChart.Data> chartData = FXCollections.observableArrayList(data);
+
+        PieChart chart = new PieChart(data);
+
         //Populate
-        chart.setData(chartData);
+        chart.setTitle("Watched Movies");
+        chart.setLabelsVisible(true);
+        chart.setLegendVisible(false);
+        return chart;
     }
 }
