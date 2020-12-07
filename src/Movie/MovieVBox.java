8d61693f8d33a8b09795ee14f538094ade24cd1d
Trevor Slobodnick <W0740591@myscc.ca>
2020-12-07 00:06:47 -0500

    Added my javadoc comments

--- src/Movie/MovieVBox.java
+++ src/Movie/MovieVBox.java
@@ -2,13 +2,25 @@ package Movie;
 
 import javafx.scene.layout.VBox;
 
+/**
+ * Custom Vbox that stores movie objects
+ * @author Trevor Slobodnick
+ * */
 public class MovieVBox extends VBox {
     private Movie movie;
 
+    /**
+     * Create a VBox that stores a movie object
+     * @param movie The movie to add to this VBox
+     * */
     public MovieVBox(Movie movie) {
         this.movie = movie;
     }
 
+    /**
+     * Gets movie object associated with vbox
+     * @return Movie
+     * */
     public Movie getMovie() {
         return movie;
     }
